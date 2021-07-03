11f80b4f585ba2ce488b15d6db25650f522228e8
Igor Lasota <igorlasota07@gmail.com>
2021-07-03 14:09:10 +0200

    Added Databse Connection

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Databse Connected"));
+
+app.use(express.json());
+
+app.listen(3000, console.log("Server Started"));
