28a64b7166594b7c6cfccf98980d5c885497591f
Igor Lasota <igorlasota07@gmail.com>
2021-07-05 13:33:33 +0200

    Changed request handling in user.post

--- routes/users.js
+++ routes/users.js
@@ -17,14 +17,17 @@ router.post("/", async (req, res) => {
   const user = new User({
     _id: req.body.userName.replace(/ /g, ""),
     userName: req.body.userName,
-    auth: req.body.auth,
+    auth: {
+      password: req.body.password,
+      email: req.body.email,
+    },
   });
 
   try {
     const newUser = await user.save();
     res.status(201).json(newUser);
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(401).json({ message: err.message });
   }
 });
 
